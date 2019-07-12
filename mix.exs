3f3a39067b98c3c636c732a9e8a7018624eb763a
Alexei Mikhailov <amikhailov83@gmail.com>
2019-07-12 15:49:27 +0300

    Add Elixir 1.8 support
    - Time unit warnings are fixed
    - Elixir 1.8 is added to test matrix

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Freddy.Mixfile do
     [
       app: :freddy,
       version: "0.15.1",
-      elixir: "~> 1.5",
+      elixir: "~> 1.6",
       elixirc_paths: elixirc_paths(Mix.env()),
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
