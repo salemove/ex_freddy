6b30665ed543fdfa8a2fc3be229fe6b14cd77063
Alexei Mikhailov <amikhailov83@gmail.com>
2020-12-18 05:34:31 +0200

    Bump version to 0.16.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Freddy.Mixfile do
   def project do
     [
       app: :freddy,
-      version: "0.15.2",
+      version: "0.16.0",
       elixir: "~> 1.6",
       elixirc_paths: elixirc_paths(Mix.env()),
       build_embedded: Mix.env() == :prod,
